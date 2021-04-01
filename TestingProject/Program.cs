b49f1a7566efa136506696f998f14e713ec2f56d
andrei-stanimir <andrei.stanimir@avira.com>
2021-04-01 13:02:54 +0300

    init

--- TestingProject/Program.cs
+++ TestingProject/Program.cs
@@ -1,69 +1,157 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace TestingProject
 {
     internal class Program
     {
-        
-        public class Interval
-        {
-            public int Start;
-            public int Final;
+        private const string PATH = @"C:\Users\andrei.stanimir\source\repos\TestingProject\TestingProject\input_files\";
 
-            public Interval(int start, int final)
+        public class Problema
+        {
+            private int necessaryIntervalLength;
+            private int n;
+            private int m;
+            private List<Interval> intervaleStudent;
+            private List<Interval> intervaleProfesor;
+
+            public Problema(int k, int n, int m, List<Interval> intervaleStudent, List<Interval> intervaleProfesor)
             {
-                Start = start;
-                Final = final;
-            }
-        }
-        public class IntervalComparer : Comparer<Interval>
-        {
-            public override int Compare(Interval x, Interval y)
-            {
-                return x.Start<y.Start;
-            }
-        }
-
-
-        private static void Solve(string path)
-        {
-            ReadFile(path);
-
-        }
-
-        private static void ReadFile(string path)
-        {
-            StreamReader reader = new StreamReader(path);
-            var k = int.Parse(reader.ReadLine());
-            var n = int.Parse(reader.ReadLine());
-
-            List<Interval> intervaleStudent = new List<Interval>(n);
-
-            for (int i = 0; i < n; i++)
-            {
-                string[] bounds = reader.ReadLine().Split(' ');
-                Interval interval = new Interval(int.Parse(bounds[0]), int.Parse(bounds[1]));
-                intervaleStudent[i] = interval;
+                this.necessaryIntervalLength = k;
+                this.n = n;
+                this.m = m;
+                this.intervaleStudent = intervaleStudent;
+                this.intervaleProfesor = intervaleProfesor;
             }
 
-            var m = int.Parse(reader.ReadLine());
-            List<Interval> intervaleProfesor = new List<Interval>(n);
-
-            for (int i = 0; i < m; i++)
+            public Problema()
             {
-                string[] bounds = reader.ReadLine().Split(' ');
-                Interval interval = new Interval(int.Parse(bounds[0]), int.Parse(bounds[1]));
-                intervaleProfesor[i] = interval;
             }
 
-            intervaleProfesor.Sort()
+            public void ReadFile(string path)
+            {
+                path = PATH + path;
+                if (!File.Exists(path))
+                {
+                    throw new FileNotFoundException();
+                }
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    if (reader.EndOfStream)
+                    {
+                    }
+                    necessaryIntervalLength = int.Parse(reader.ReadLine());
+
+                    n = int.Parse(reader.ReadLine());
+                    intervaleStudent = new List<Interval>(n);
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        string[] bounds = reader.ReadLine().Split(' ');
+                        Interval interval = new Interval(int.Parse(bounds[0]), int.Parse(bounds[1]));
+                        intervaleStudent.Add(interval);
+                    }
+
+                    m = int.Parse(reader.ReadLine());
+                    intervaleProfesor = new List<Interval>(m);
+                    for (int i = 0; i < m; i++)
+                    {
+                        string[] bounds = reader.ReadLine().Split(' ');
+                        Interval interval = new Interval(int.Parse(bounds[0]), int.Parse(bounds[1]));
+                        intervaleProfesor.Add(interval);
+                    }
+                    intervaleStudent.OrderBy(i => i.Start).ThenBy(i => i.Final);
+                    intervaleProfesor.OrderBy(i => i.Start).ThenBy(i => i.Final);
+                }
+            }
+
+            public void PrintIntervale()
+            {
+                intervaleStudent.ToList().ForEach(i => Console.WriteLine($"{i.Start}, {i.Final}"));
+                Console.WriteLine();
+                intervaleProfesor.ToList().ForEach(i => Console.WriteLine($"{i.Start}, {i.Final}"));
+            }
+
+            public class Interval
+            {
+                public int Start;
+                public int Final;
+
+                public Interval(int start, int final)
+                {
+                    Start = start;
+                    Final = final;
+                }
+
+                public int GetDuration()
+                {
+                    return Final - Start;
+                }
+
+                public override string ToString()
+                {
+                    return Start + ", " + Final;
+                }
+            }
+
+            public class IntervalComparer : Comparer<Interval>
+            {
+                public override int Compare(Interval x, Interval y)
+                {
+                    if (x.Start < y.Start)
+                        return -1;
+                    if (x.Start > y.Start)
+                        return 1;
+                    if (x.Final < y.Final)
+                        return -1;
+                    if (x.Final > y.Final)
+                        return 1;
+                    return 0;
+                }
+            }
+
+            public Interval? Solve()
+            {
+                int iStudent = 0;
+                int iProfesor = 0;
+                while (iStudent < n && iProfesor < m)
+                {
+                    while (intervaleStudent[iStudent].GetDuration() < necessaryIntervalLength)
+                        iStudent++;
+                    while (intervaleProfesor[iProfesor].GetDuration() < necessaryIntervalLength)
+                        iProfesor++;
+                    Interval student = intervaleStudent[iStudent];
+                    Interval profesor = intervaleProfesor[iProfesor];
+                    var x = Math.Max(intervaleStudent[iStudent].Start, intervaleProfesor[iProfesor].Start);
+                    var y = Math.Min(intervaleStudent[iStudent].Final, intervaleProfesor[iProfesor].Final);
+
+                    if (y - x >= necessaryIntervalLength)
+                    {
+                        return new Interval(x, x + necessaryIntervalLength);
+                    }
+                    if (student.Final < profesor.Final)
+                        iStudent += 1;
+                    else if (student.Final > profesor.Final)
+                        iProfesor += 1;
+                    else
+                    {
+                        iStudent += 1;
+                        iProfesor += 1;
+                    }
+                }
+                return null;
+            }
         }
 
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var problema = new Problema();
+            problema.ReadFile("test_baza.in");
+            Console.WriteLine(problema.Solve());
+            problema.PrintIntervale();
         }
     }
 }
